6f193bc15ed3bdf33631f9eba69bce6f5dde023d
technobeas <gamingtechno456@gmail.com>
2025-09-27 19:35:29 +0530

    updated

--- public/shared.js
+++ public/shared.js
@@ -1,10 +1,24 @@
+// async function getMe() {
+//   const res = await fetch('/api/me');
+//   const data = await res.json();
+//   return data.user;
+// }
+
 async function getMe() {
-  const res = await fetch('/api/me');
+  const res = await fetch("/api/me", { credentials: "include" });
   const data = await res.json();
   return data.user;
 }
 
+// async function logout() {
+//   await fetch("/api/logout", { method: "POST" });
+//   window.location.href = "/login.html";
+// }
+
 async function logout() {
-  await fetch('/api/logout', { method: 'POST' });
-  window.location.href = '/login.html';
+  await fetch("/api/logout", {
+    method: "POST",
+    credentials: "include",
+  });
+  window.location.href = "/index.html";
 }
