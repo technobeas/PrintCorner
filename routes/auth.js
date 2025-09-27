a77eee955883c50cdf47ca40a13fc2473fcb4a46
technobeas <gamingtechno456@gmail.com>
2025-09-27 17:43:12 +0530

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -31,7 +31,7 @@ router.post("/register", async (req, res) => {
 });
 
 // LOGIN (admin or user) with session regeneration
-router.post("/index", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password)
