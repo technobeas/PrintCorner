7f4e30db35f5be990f01105db9b976a97b0e5dad
technobeas <gamingtechno456@gmail.com>
2025-09-27 17:37:11 +0530

    updated

--- server.js
+++ server.js
@@ -437,7 +437,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Route fallbacks
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/admin.html", (req, res) => {
